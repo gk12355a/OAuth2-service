3d1be339f3378a7072894cd5169638758d2a0353
KienDoTri <kiendotri@gmail.com>
2025-12-10 04:09:03 +0700

    del env

--- auth-service/src/main/java/com/cmc/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/cmc/auth/AuthServiceApplication.java
@@ -1,16 +1,11 @@
 package com.cmc.auth;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
